4fbad647da2438b4a5042edb6535c764d3fa3230
alanbarco <alaxbarc@espol.edu.ec>
2022-07-15 10:45:35 -0500

    Avances proyecto 2

--- Proyecto 2/js/equipo.js
+++ Proyecto 2/js/equipo.js
@@ -2,40 +2,43 @@ window.addEventListener('DOMContentLoaded', event => {
     // Simple-DataTables
     // https://github.com/fiduswriter/Simple-DataTables/wiki
     
-    fetch("https://www.balldontlie.io/api/v1/teams")
+    fetch("https://www.balldontlie.io/api/v1/players")
         .then(response => response.json())
         .then(data => {
             let datos = data['data'];
             let select = document.querySelector('div.selector > select');
-            let equipo, abreviacion, ciudad, posicion, conferencia,division;
-            for(var elemento of datos){
-                equipo = elemento['full_name'];
-                let plantillaEquipo = `<option value= "${equipo}">${equipo}</option>`;
-                select.innerHTML += plantillaEquipo;
-            }            
+            for(let elemento of datos){
+                let id = elemento['id'];
+                let nombre = elemento['first_name'];
+                let apellido = elemento['last_name'];
+                let plantilla = `<option value= "${id}">${nombre} ${apellido}</option>`;
+                select.innerHTML += plantilla;
+            }          
+        }).catch(console.error);
             select.addEventListener("change",function(){
-                fetch("https://www.balldontlie.io/api/v1/teams")
+                fetch("https://www.balldontlie.io/api/v1/players")
                 .then(response => response.json())
                 .then(data => {
                     let datos = data['data'];
-
-                    datos.filter(dato => dato['full_name'] == select.value).forEach(dato => {
-                        let abreviacion = dato['abbreviation'];
-                        document.getElementById('abreviacion').innerHTML = abreviacion;
-                        let ciudad = dato['city'];
+                    //document.querySelector('div.datos').innerHTML='';
+                    datos.filter(dato => dato['id'] == select.value).forEach(dato => {
+                        console.log(dato['id'])
+                        document.getElementById("nombre").innerHTML = dato['first_name'] +' '+ dato['last_name'];
+                        document.getElementById("equipo").innerHTML = dato['team']['full_name'];
+                        let ciudad = dato['team']['city'];
                         document.getElementById('ciudad').innerHTML = ciudad;
-                        let confe = dato['conference'];
-                        document.getElementById('conferencia').innerHTML = confe;
-                        let div = dato['division'];
-                        document.getElementById('division').innerHTML = div;
-                      });
+                        console.log(dato['position'])
+                        document.getElementById("posicion").innerHTML = dato['position'];
+                        if(dato['position'] == ''){
+                            document.getElementById('posicion').innerHTML = '--';
+                        }
+                         });
                 }).catch(console.error);
                 
-                let docNombre = document.getElementById("nombreEquipo");
-                docNombre.innerHTML=select.value;
-            })
+                
+            });
             
-        }).catch(console.error);
+        
 
         
 });
